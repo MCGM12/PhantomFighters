831e328f9e8afa91ec743f1df90e5313cbd38ae3
MCGM12 <matocraft506@gmail.com>
2021-02-10 14:35:03 -0800

    Update 2D Renderer.asset, 2D Renderer.asset.meta, and 16 more files...

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    Vector3 transf;
+    
+    void Update()
+    {
+        transf.x = GameObject.FindWithTag("Player").transform.position.x;
+        transf.y = GameObject.FindWithTag("Player").transform.position.y;
+        transf.z = -10f;
+ 
+        transform.position = transf;
+    }
+}
