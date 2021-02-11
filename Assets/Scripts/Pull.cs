c04f5c2e0eb89f77d273283dba0a5ac71c96d9a4
JaredNo <jar091002@yahoo.com>
2021-02-10 17:42:27 -0800

    Test web push working

---
+++ Assets/Scripts/Pull.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Pull : MonoBehaviour
+{
+    public GameObject mainPlayer;
+
+    //https://docs.unity3d.com/ScriptReference/Networking.UnityWebRequest.Get.html
+    string URL = "http://vgdapi.basmati.org/gets4.php?groupid=DUMMY&row=7";
+
+    void Start()
+    {
+        string m_Path;
+        //Get the path of the Game data folder
+        m_Path = Application.dataPath;
+
+        //Output the Game data path to the console
+        Debug.Log("dataPath : " + m_Path);
+
+        // A non-existing page.
+        // StartCoroutine(GetRequest("https://error.html"));
+    }
+
+    private void OnMouseDown()
+    {
+        // A correct website page.
+        Debug.Log("Calling Website");
+        StartCoroutine(GetRequest(URL));
+    }
+
+    private void MovePlayer(string webText)
+    {
+        Debug.Log("Moving player..." + webText);
+        //Data comes in ROW,X|Y
+        string[] webData = webText.Split(',');
+        string[] pos = webData[1].Split('|');
+        float x = float.Parse(pos[0]);
+        float y = float.Parse(pos[1]);
+        Debug.Log(x.ToString() + "," + y.ToString());
+        mainPlayer.transform.position = new Vector3(x, y, 0);
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(pages[page] + ": Error: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                MovePlayer(webRequest.downloadHandler.text);
+                
+            }
+        }
+
+        
+    }
+
+
+}
---
+++ Assets/Scripts/Push.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Push : MonoBehaviour
+{
+    public GameObject statue;
+
+    string URL = "http://vgdapi.basmati.org/mods4.php";
+
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("PUSHING DATA....");
+            statue.transform.position = transform.position;
+            StartCoroutine(Upload());
+            Debug.Log(statue.transform.position);
+        }
+    }
+
+    IEnumerator Upload()
+    {
+        WWWForm form = new WWWForm();
+       // form.AddField("myField", "myData");
+        form.AddField("groupid", "pm38");
+        form.AddField("grouppw", "2yy67vZFEU");
+        form.AddField("row", 0);
+        string statXY = statue.transform.position.x.ToString() + "|" + statue.transform.position.y.ToString();
+
+        form.AddField("s4", "test");
+
+        using (UnityWebRequest www = UnityWebRequest.Post(URL, form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+            }
+        }
+    }
+}
