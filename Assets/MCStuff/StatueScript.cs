197ec1afb8f64574e1a5d94c86db0f50e3ec3e7b
MCGM12 <matocraft506@gmail.com>
2021-03-23 12:54:17 -0700

    Update StatueScript.cs, LightingTest.unity, and 278 more files...

--- Assets/MCStuff/StatueScript.cs
+++ Assets/MCStuff/StatueScript.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class StatueScript : MonoBehaviour
 {
-  
+    public float Statue1Timer;
+    public float statTimer;
+    public bool timer;
+    public Transform stat;
+    public Transform player;
 
     void Start()
     {
-        
+        statTimer = 5f;
     }
 
     void Update()
     {
-        
+        if(timer == false)
+        {
+            timer = true;
+            StartCoroutine("Timer(statTimer)");
+        }
+    }
+
+    private IEnumerable Timer(float time)
+    {
+        yield return new WaitForSeconds(time);
+        stat.position = player.position;
+        timer = false;
     }
 
  
